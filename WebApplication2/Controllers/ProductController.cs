47a0556193aa7c7a49afa91acad5c70b443e1cbd
Brandon <s01100303@acad.tri-c.edu>
2019-01-27 20:41:51 -0500

    Created Week 2 File part B

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application_Week_2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        public string Details(int id)
+        {
+            string message = "Details displayed for ID = " + id;
+            return message;
+        }
+
+        public string Location(string zip)
+        {
+            string message = HttpUtility.HtmlEncode("Location displayed for zip = " + zip);
+            return message;
+        }
+    }
+}
