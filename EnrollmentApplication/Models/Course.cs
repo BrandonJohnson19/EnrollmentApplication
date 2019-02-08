58714152761a349fe2bd2ce6136f737f32cd2b25
Brandon <s01100303@acad.tri-c.edu>
2019-02-08 00:18:20 -0500

    Week 4 Assignment

---
+++ EnrollmentApplication/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class Course
+    {
+        public virtual int CourseId { get; set; }
+        public virtual String Title { get; set; }
+        public virtual String Description { get; set; }
+        public virtual int CourseCredits { get; set; }
+    }
+}
